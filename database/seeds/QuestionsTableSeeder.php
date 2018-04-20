2072e5026f8d662625ba51072d50adf4cb9037c6
mandar <vyasmandar1434@gmail.com>
2018-04-20 16:26:47 -0400

    added questionview to display the answers to question in the sidebar.

--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Database\Seeder;
-
 class QuestionsTableSeeder extends Seeder
 {
     /**
@@ -10,17 +8,14 @@ class QuestionsTableSeeder extends Seeder
      * @return void
      */
     public function run()
-
-        {
-            $users = App\User::all();
-            for ($i = 1; $i <= 16; $i++) {
-                $users->each(function ($user) {
-                    $question = factory(\App\Question::class)->make();
-                    $question->user()->associate($user);
-                    $question->save();
-
-                });
-            }
-
+    {
+        $users = App\User::all();
+        for ($i = 1; $i <= 16; $i++) {
+            $users->each(function ($user) {
+                $question = factory(\App\Question::class)->make();
+                $question->user()->associate($user);
+                $question->save();
+            });
         }
     }
+}
